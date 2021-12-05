a8e5b33172371de88ed19825f9305bc6ec83fd00
Daria V <voronovadaria@gmail.com>
2021-12-05 16:15:17 +0300

    4.1. JSON формат. [#238554]

--- src/main/java/ru/job4j/dreamjob/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dreamjob/servlet/GreetingServlet.java
@@ -1,21 +1,44 @@
 package ru.job4j.dreamjob.servlet;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.job4j.dreamjob.model.Email;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GreetingServlet extends HttpServlet {
 
+    private final List<Email> emails = new CopyOnWriteArrayList<>();
+    private static final Gson GSON = new GsonBuilder().create();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        res.setContentType("text/plain");
-        res.setCharacterEncoding("UTF-8");
-        String name = req.getParameter("name");
-        PrintWriter writer = new PrintWriter(res.getOutputStream());
-        writer.println("Nice to meet you, " + name);
-        writer.flush();
+        res.setContentType("application/json; charset=utf-8");
+        OutputStream output = res.getOutputStream();
+        String json = GSON.toJson(emails);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        Email email = GSON.fromJson(req.getReader(), Email.class);
+        emails.add(email);
+        res.setContentType("application/json; charset=utf-8");
+        OutputStream output = res.getOutputStream();
+        String json = GSON.toJson(email);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
     }
 }
